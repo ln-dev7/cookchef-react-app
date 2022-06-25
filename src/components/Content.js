1811e8671a5d36566b0aa5fc1ea874b30f75cb04
ln-dev7 <leonelngoya@gmail.com>
2022-06-25 09:37:02 +0100

    Style de base du content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,25 @@
+import styles from "./Content.module.scss";
+
 function Content() {
-    return <h1 className="flex-fill b3">Content</h1>;
-  }
-  
-  export default Content;
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
