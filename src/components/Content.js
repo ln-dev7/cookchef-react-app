a672d3894c5f829e2a0e467330071b610d746306
ln-dev7 <leonelngoya@gmail.com>
2022-06-25 09:45:39 +0100

    Style des recettes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,21 +1,23 @@
-import styles from "./Content.module.scss";
+import styles from './Content.module.scss';
+import Recipe from './Recipe';
 
 function Content() {
   return (
     <div className="flex-fill container p-20">
-      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <h1 className="my-30">Découvrez nos nouvelles 
+recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
         </div>
       </div>
     </div>
